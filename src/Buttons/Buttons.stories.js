e13f2604ecca8adc8b6f3dede835d5dfb460cf8c
wfercanas <fernando.canas@outlook.com>
2022-12-10 11:20:09 -0500

    test: test interaction in dummy button

--- src/Buttons/Buttons.stories.js
+++ src/Buttons/Buttons.stories.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Button } from ".";
 
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 const story = {
   title: "Buttons",
   components: [Button],
@@ -9,6 +12,16 @@ const story = {
 const Primary = (args) => <Button {...args} />;
 Primary.args = {
   label: "Button",
+  href: "http://storybook.js.org",
+};
+
+Primary.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("button"));
+  expect(canvas.getByRole("button")).toHaveAttribute(
+    "href",
+    "http://storybook.js.org"
+  );
 };
 
 export default story;
