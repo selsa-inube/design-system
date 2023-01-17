f3e78a666abf7eb05a44be396a61316ec66aefaf
wfercanas <fernando.canas@outlook.com>
2023-01-17 15:09:05 -0500

    feat: basic shared colors structure

---
+++ src/shared/colors.js
@@ -0,0 +1,24 @@
+const ref = {
+  palette: {
+    blue: {
+      b50: "#DEEBFF",
+      b75: "#B3D4FF",
+      b700: "",
+    },
+    yellow: {},
+  },
+};
+
+const sys = {
+  actions: {
+    primary: ref.palette.blue.b700,
+  },
+  messages: {},
+};
+
+const colors = {
+  ref,
+  sys,
+};
+
+export { colors };
