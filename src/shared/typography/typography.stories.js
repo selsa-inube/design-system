76b003f35b55d717a241d21e9c475e9d40beab55
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-01-19 08:54:14 -0500

    docs(typography.stories): implemented documentation of methods and variables

--- src/shared/typography/typography.stories.js
+++ src/shared/typography/typography.stories.js
@@ -1,33 +1,46 @@
 import React from "react";
-
-import { typography } from "./typography";
 import styled from "styled-components";
 
+import { typography } from "./typography";
+
+/**
+ * history of typography tokens (system and reference)
+ */
 const story = {
   title: "typography",
   components: [typography],
 };
 
 /**
- * div contenedor creado con styled-components
+ * div container created with styled-components
  */
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(100%, 1fr));
 `;
 
+/**
+ * div card created with styled-components
+ */
 const Div = styled.div`
   border: 2px solid;
   margin: 9px;
   padding-left: 10px;
 `;
 
+/**
+ * element <p> created with styled-components to display the tokens
+ */
 const P = styled.p`
   font-family: "${typography.ref.typeface.brand}", sans-serif;
   overflow: hidden;
   text-overflow: ellipsis;
 `;
 
+/**
+ * The TypographyRef function is a React component that is responsible for displaying a list of all the properties and values of the typography object.
+ * @returns cards with token information
+ */
 const TypographyRef = () => {
   const createList = (obj, parentKey) => {
     return Object.entries(obj).map(([key, value]) => {
