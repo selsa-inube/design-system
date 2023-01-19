ab25797560eaeb7e15be9034fd405b7276c6eaa2
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-01-19 00:05:06 -0500

    feat: added the system token reference table

--- src/shared/colors/stories/index.jsx
+++ src/shared/colors/stories/index.jsx
@@ -5,48 +5,93 @@ import {
   StyledDivFlexInferior,
   StyledDivInfoCard,
   StyledGlobalGrid,
+  StyledGlobalGridTokens,
+  StyledColor,
+  StyledTable,
+  StyledTableHead,
+  StyledTableTh,
+  StyledTableTd,
+  StyledTableSubTd,
+  StyledHeader,
 } from "./styles";
-import { TinyColor } from "@ctrl/tinycolor";
 
 const Colors = (dataColors) => {
   return (
-    <StyledGlobalGrid>
-      {Object.entries(dataColors.colors.ref.palette).map(([name, colors]) => (
-        <div key={name}>
-          <StyledGrid>
-            {Object.entries(colors).map(([colorName, colorValue]) => (
-              <StyledCard key={colorName}>
-                <div
-                  style={{
-                    backgroundColor: colorValue,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    color: new TinyColor(colorValue).isDark()
-                      ? "#FFFFFF"
-                      : "#000000",
-                  }}
-                >
-                  Lorem Ipsum
-                </div>
-                <StyledDivFlexInferior>
-                  <StyledDivInfoCard>
-                    <label htmlFor="name">Name</label>
-                    <span id="name">{colorName}</span>
-                  </StyledDivInfoCard>
-                  <StyledDivInfoCard>
-                    <label htmlFor="hex">Hex</label>
-                    <span id="hex">{colorValue}</span>
-                  </StyledDivInfoCard>
-                </StyledDivFlexInferior>
-              </StyledCard>
-            ))}
-          </StyledGrid>
-        </div>
-      ))}
-    </StyledGlobalGrid>
+    <>
+      <StyledHeader>
+        <h1>Colors</h1>
+        <p>
+          Color palette with the respective scales and the different use cases
+        </p>
+      </StyledHeader>
+      <StyledGlobalGridTokens>
+        <StyledGlobalGrid>
+          {Object.entries(dataColors.colors.ref.palette).map(
+            ([name, colors]) => (
+              <div key={name}>
+                <StyledGrid>
+                  {Object.entries(colors).map(([colorName, colorValue]) => (
+                    <StyledCard key={colorName}>
+                      <StyledColor color={colorValue}>Lorem Ipsum</StyledColor>
+                      <StyledDivFlexInferior>
+                        <StyledDivInfoCard>
+                          <label htmlFor="name">Name</label>
+                          <span id="name">{colorName}</span>
+                        </StyledDivInfoCard>
+                        <StyledDivInfoCard>
+                          <label htmlFor="hex">Hex</label>
+                          <span id="hex">{colorValue}</span>
+                        </StyledDivInfoCard>
+                      </StyledDivFlexInferior>
+                    </StyledCard>
+                  ))}
+                </StyledGrid>
+              </div>
+            )
+          )}
+        </StyledGlobalGrid>
+        <StyledTable>
+          <StyledTableHead>
+            <tr>
+              <StyledTableTh>Role</StyledTableTh>
+              <StyledTableTh>System token</StyledTableTh>
+              <StyledTableTh>Reference token</StyledTableTh>
+              <StyledTableTh>Default value</StyledTableTh>
+            </tr>
+          </StyledTableHead>
+          {Object.entries(dataColors.colors.sys).map(([name, colors]) => (
+            <tbody>
+              <tr>
+                <StyledTableTd>
+                  <h1>{name}</h1>
+                </StyledTableTd>
+                <StyledTableTd>
+                  {Object.entries(colors).map(([colorName]) => (
+                    <StyledTableSubTd>
+                      sys.{name}.{colorName}
+                    </StyledTableSubTd>
+                  ))}
+                </StyledTableTd>
+                <StyledTableTd>
+                  {Object.entries(colors).map(([colorName]) => (
+                    <StyledTableSubTd>
+                      sys.{name}.{colorName}
+                    </StyledTableSubTd>
+                  ))}
+                </StyledTableTd>
+                <StyledTableTd>
+                  {Object.entries(colors).map(([colorName, colorValue]) => (
+                    <StyledTableSubTd>
+                      <StyledColor color={colorValue}>{colorValue}</StyledColor>
+                    </StyledTableSubTd>
+                  ))}
+                </StyledTableTd>
+              </tr>
+            </tbody>
+          ))}
+        </StyledTable>
+      </StyledGlobalGridTokens>
+    </>
   );
 };
 
--- src/shared/colors/stories/styles.js
+++ src/shared/colors/stories/styles.js
@@ -1,11 +1,30 @@
 import styled from "styled-components";
+import { TinyColor } from "@ctrl/tinycolor";
+
+const StyledGlobalGridTokens = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  @media (max-width: 1130px) {
+    margin-left: 8rem;
+    grid-template-columns: repeat(1, 1fr);
+    grid-template-rows: 2fr;
+  }
+`;
 
 const StyledGlobalGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 32px;
-  grid-row-gap: 38px;
+  gap: 1rem;
+  grid-auto-rows: 22rem;
+  grid-template-columns: repeat(auto-fill, minmax(1fr));
+  @media (min-width: 2560px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    grid-column-gap: 32px;
+    grid-row-gap: 38px;
+  }
 `;
 
 const StyledGrid = styled.div`
@@ -27,6 +46,19 @@ const StyledCard = styled.div`
   margin: 0 0 10px 0;
 `;
 
+const StyledColor = styled.div`
+  background-color: ${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+     color: ${
+       new TinyColor((props) => props.color).isDark() ? "#FFFFFF" : "#000000"
+     }
+   }}
+`;
+
 const StyledDivFlexInferior = styled.div`
   background-color: #ffffff;
   color: black;
@@ -45,10 +77,88 @@ const StyledDivInfoCard = styled.div`
   flex-direction: column;
 `;
 
+const StyledTable = styled.table`
+  width: 100%;
+  background-color: #ffffff;
+  overflow-x: auto;
+  border-collapse: collapse;
+  border-width: 1px;
+  border-color: #b3bac5;
+  border-style: solid;
+  color: #000000;
+  border-radius: 10px;
+`;
+const StyledHeader = styled.header`
+  background-color: #deebff;
+  margin: -1rem -1rem 1rem -1rem;
+  display: flex;
+  flex-direction: column;
+  h1,
+  p {
+    padding: 2px 2px 2px 20px;
+  }
+  @media (max-width: 768px) {
+    margin: -1rem -3.2rem 1rem -1rem;
+  }
+  @media (max-width: 425px) {
+    margin: -1rem -25rem 1rem -1rem;
+  }
+  @media (max-width: 375px) {
+    margin: -1rem -28rem 1rem -1rem;
+  }
+  @media (max-width: 320px) {
+    margin: -1rem -31.5rem 1rem -1rem;
+  }
+`;
+const StyledTableHead = styled.thead`
+  background-color: #deebff;
+  border-radius: 1em;
+`;
+const StyledTableTh = styled.th`
+  border-width: 1px;
+  border-color: #b3bac5;
+  border-style: solid;
+  padding: 5px;
+  color: #42526e;
+`;
+const StyledTableTd = styled.td`
+  border-width: 1px;
+  border-color: #b3bac5;
+  border-style: solid;
+  padding: 5px;
+`;
+
+const StyledTableSubTd = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-width: 1px;
+  border-color: #b3bac5;
+  border-bottom: 0.5px solid #b3bac5;
+  padding: 5px;
+  height: 8rem;
+  align-items: center;
+  justify-content: center;
+  &: last-child {
+    border-bottom: none;
+  }
+  @media (min-width: 2560px) {
+    width: 405px;
+    height: 52px;
+  }
+`;
+
 export {
   StyledGrid,
   StyledCard,
   StyledDivFlexInferior,
   StyledDivInfoCard,
   StyledGlobalGrid,
+  StyledColor,
+  StyledGlobalGridTokens,
+  StyledTable,
+  StyledTableHead,
+  StyledTableTh,
+  StyledTableTd,
+  StyledTableSubTd,
+  StyledHeader,
 };
