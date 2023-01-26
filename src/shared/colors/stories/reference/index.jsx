ea074bfd00904ba568e4e4fab207bec89aaf292a
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-01-26 12:18:45 -0500

    fix(colors): changed in the name of the variables to be more precise in the use of the same

--- src/shared/colors/stories/reference/index.jsx
+++ src/shared/colors/stories/reference/index.jsx
@@ -66,11 +66,12 @@ const GridColor = (props) => {
  * It takes in a data object, and returns a styled flexbox with a grid of colors
  * @returns A styled component that is a flexbox with a grid of colors.
  */
-const ColorReference = (dataColors) => {
+const ColorReference = (props) => {
+  const { colors } = props;
   return (
     <StyledGlobalFlex>
-      {Object.keys(dataColors.colors.ref.palette).map((color) => (
-        <GridColor key={color} color={color}></GridColor>
+      {Object.keys(colors.ref.palette).map((colorGroup) => (
+        <GridColor key={colorGroup} color={colorGroup}></GridColor>
       ))}
     </StyledGlobalFlex>
   );
