6ab0452f8a6c2c32db498054c2b2773b2e1dec40
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-02-08 14:47:50 -0500

    feat(switch): added the default story

--- src/components/inputs/Switch/index.jsx
+++ src/components/inputs/Switch/index.jsx
@@ -7,18 +7,47 @@ import { StyledContainer, StyledInput, StyledSpan } from "./styles";
 
 const sizes = ["large", "small"];
 const defaultSize = "small";
+const MdDoneStyles = {
+  position: "absolute",
+  width: "10px",
+  height: "10px",
+  left: "5px",
+  top: "3px",
+};
+const MdCloseStyles = {
+  position: "absolute",
+  width: "10px",
+  height: "10px",
+  left: "17px",
+  top: "3px",
+};
 
 const Switch = (props) => {
   const { id, isDisabled, name, value, handleChange, size } = props;
   const transformedSize = sizes.includes(size) ? size : defaultSize;
+  const [isToggled, setToggled] = useState(value);
 
-  console.log("size ", size);
+  const handleToggle = () => {
+    setToggled(!isToggled);
+    handleChange(!isToggled);
+  };
+
   return (
     <StyledContainer id={id} size={transformedSize}>
-      <StyledInput type="checkbox" size={transformedSize} />
+      <StyledInput
+        type="checkbox"
+        size={transformedSize}
+        defaultChecked={value}
+        onChange={handleToggle}
+        disabled={isDisabled}
+        name={name}
+      />
       <StyledSpan size={transformedSize}>
-        <MdDone color="white" />
-        <MdClose color="white" />
+        {isToggled === true ? (
+          <MdDone color="white" style={MdDoneStyles} />
+        ) : (
+          <MdClose color="white" style={MdCloseStyles} />
+        )}
       </StyledSpan>
     </StyledContainer>
   );
--- src/components/inputs/Switch/stories/Switch.Default.stories.js
+++ src/components/inputs/Switch/stories/Switch.Default.stories.js
@@ -14,52 +14,63 @@ const story = {
 };
 
 const sizes = ["large", "small"];
-const tokensSpinner = [
-  "blue",
-  "green",
-  "yellow",
-  "red",
-  "purple",
-  "dark",
-  "white",
-];
-const transparent = [false, true];
 const logState = (state) => {
   console.log("Toggled:", state);
 };
-export const Default = (args) => <Switch {...args} />;
+const DefaultTemplate = ({ ...args }) => <Switch {...args} />;
+export const Default = DefaultTemplate.bind({});
+//export const Default = (args) => <Switch {...args} />;
 Default.args = {
-  value: true,
+  id: "thisIsAnId",
+  value: false,
   size: "small",
   handleChange: logState,
+  isDisabled: false,
 };
-// Default.argTypes = {
-//   size: {
-//     options: sizes,
-//     control: { type: "select" },
-//     description:
-//       "different sizes that the component can have, according to the need",
-//     table: {
-//       defaultValue: { summary: "medium" },
-//     },
-//   },
-//   appearance: {
-//     options: tokensSpinner,
-//     control: { type: "select" },
-//     description: "colors used to identify the state of the component",
-//     table: {
-//       defaultValue: { summary: "blue" },
-//     },
-//   },
-//   isTransparent: {
-//     options: transparent,
-//     control: { type: "boolean" },
-//     description:
-//       "indicates when the background of the spinner should have color",
-//     table: {
-//       defaultValue: { summary: false },
-//     },
-//   },
-// };
+Default.argTypes = {
+  id: {
+    options: "",
+    control: { type: "select" },
+    description:
+      "this element can have a label on it, so this id allows us to connect the label with the switch",
+  },
+  isDisabled: {
+    options: [true, false],
+    control: { type: "boolean" },
+    description:
+      "if the switch is disabled or not. This prevents any interaction.",
+    table: {
+      defaultValue: { summary: "false" },
+    },
+  },
+  name: {
+    options: "",
+    control: { type: "select" },
+    description:
+      "descriptive name for value property to be submitted in a form",
+  },
+  value: {
+    options: [true, false],
+    control: { type: "boolean" },
+    description: "value to be submitted in a form.",
+    table: {
+      defaultValue: { summary: "false" },
+    },
+  },
+  handleChange: {
+    options: "",
+    control: { type: "func" },
+    description:
+      "is a function that the component receives and that can be executed every time the switch state is modified",
+  },
+  size: {
+    options: sizes,
+    control: { type: "select" },
+    description: "toggle size",
+    table: {
+      defaultValue: { summary: "small" },
+    },
+  },
+};
 
 export default story;
