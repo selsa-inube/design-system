7a7db8852b0c68d3aef5d27156ecb804fa24f7bb
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-02-15 16:03:08 -0500

    fix(switch): add spacing between rules css and organize it to put in the bottom the dynamic styles

--- src/components/inputs/Switch/styles.js
+++ src/components/inputs/Switch/styles.js
@@ -15,26 +15,32 @@ const sizes = {
 
 const StyledSpan = styled.span`
   position: absolute;
-  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+  transition: 0.1s;
+  border-radius: 30px;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
   background: ${(props) =>
     props.disabled
       ? colors.ref.palette.neutral.n40
       : colors.ref.palette.neutral.n200};
-  transition: 0.1s;
-  border-radius: 30px;
+
   &:hover {
     background-color: ${(props) =>
       props.disabled
         ? colors.ref.palette.neutral.n40
         : colors.ref.palette.neutral.n70};
   }
+
   &:before {
     position: absolute;
     content: "";
+    left: 2px;
+    border-radius: 50%;
+    transition: 0.3s;
+    background-color: ${colors.ref.palette.neutral.n0};
     ${(props) =>
       props.size === "small"
         ? css`
@@ -47,10 +53,6 @@ const StyledSpan = styled.span`
             height: 16px;
             bottom: 1.9px;
           `};
-    left: 2px;
-    background-color: ${colors.ref.palette.neutral.n0};
-    border-radius: 50%;
-    transition: 0.3s;
   }
 `;
 
@@ -64,11 +66,13 @@ const StyledInput = styled.input`
   opacity: 0;
   width: 0;
   height: 0;
+
   &:checked + span {
     background-color: ${(props) =>
       props.disabled
         ? colors.ref.palette.green.g75
         : colors.ref.palette.green.g400};
+
     &:hover {
       background-color: ${(props) =>
         props.disabled
@@ -76,6 +80,7 @@ const StyledInput = styled.input`
           : colors.ref.palette.green.g300};
     }
   }
+
   &:checked + span:before {
     ${(props) =>
       props.size === "small"
