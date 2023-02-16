c0e42f64e02487827bbde9ffbf8599e0fef237fd
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-02-16 11:38:42 -0500

    fix(stack): include the validations of properties and the console.warn warning

--- src/components/layouts/Stack/index.jsx
+++ src/components/layouts/Stack/index.jsx
@@ -11,22 +11,77 @@ export const flexAligments = [
   "space-evenly",
   "space-between",
 ];
+const defaultDirection = "row";
+const defaultJustifyContent = "flex-start";
+const defaultAlignItems = "flex-start";
 
 const Stack = (props) => {
   const {
     children,
     wrap = false,
-    direction = "row",
-    justifyContent = "flex-start",
-    alignItems = "flex-start",
+    direction = defaultDirection,
+    justifyContent = defaultJustifyContent,
+    alignItems = defaultAlignItems,
     gap = "0px",
   } = props;
 
+  const transformedDirection = directionAligments.includes(direction)
+    ? direction
+    : defaultDirection;
+  const transformedAlignItems = flexAligments.includes(alignItems)
+    ? alignItems
+    : defaultAlignItems;
+  const transformedJustifyContent = flexAligments.includes(justifyContent)
+    ? justifyContent
+    : defaultJustifyContent;
+
+  if (!flexAligments.includes(alignItems)) {
+    console.warn(
+      `Invalid prop value for 'alignItems': ${alignItems}. Allowed values are ${flexAligments.join(
+        ", "
+      )}.`
+    );
+  }
+
+  if (!flexAligments.includes(justifyContent)) {
+    console.warn(
+      `Invalid prop value for 'justifyContent': ${justifyContent}. Allowed values are ${flexAligments.join(
+        ", "
+      )}.`
+    );
+  }
+
+  if (!directionAligments.includes(direction)) {
+    console.warn(
+      `Invalid prop value for 'direction': ${direction}. Allowed values are ${directionAligments.join(
+        ", "
+      )}.`
+    );
+  }
+
+  if (typeof wrap !== "boolean") {
+    console.warn(
+      `Invalid prop value for 'wrap': ${wrap}. Allowed values are true or false.`
+    );
+  }
+
+  if (typeof gap !== "string") {
+    console.warn(
+      `Invalid prop value for 'wrap': ${gap}. Allowed values are a string(with a default value that must include the measure e.g 10px).`
+    );
+  }
+
+  if (typeof children !== "object") {
+    console.warn(
+      `Invalid prop value for 'children': ${children}. Allowed values are a children(React.ReactNode).`
+    );
+  }
+
   return (
     <StyledFlex
-      direction={direction}
-      justifyContent={justifyContent}
-      alignItems={alignItems}
+      direction={transformedDirection}
+      justifyContent={transformedJustifyContent}
+      alignItems={transformedAlignItems}
       wrap={+wrap}
       gap={gap}
     >
