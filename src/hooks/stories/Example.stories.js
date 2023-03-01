d6b416750ae002b197cd62525ef2ddb08d6932ff
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-01 17:26:12 -0500

    feat(useMediaQuery): added the story of example to cover the screen resize

---
+++ src/hooks/stories/Example.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch } from "../../components/inputs/Switch";
+import { SwitchController } from "./../../components/inputs/Switch/stories/SwitchController";
+import useMediaQuery from "../useMediaQuery";
+
+const story = {
+  title: "hooks/useMediaQuery",
+  components: [Switch],
+  decorators: [
+    (Story) => (
+      <div style={{ margin: "3em" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const Example = (args) => {
+  const isLargeScreen = useMediaQuery("(min-width: 600px)");
+
+  const size = isLargeScreen ? "large" : "small";
+
+  return <SwitchController {...args} size={size} />;
+};
+
+export default story;
