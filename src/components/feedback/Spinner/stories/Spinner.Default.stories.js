74aac96e45c98b679f88a922dcf8de34bea10e76
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-06 12:02:32 -0500

    fix(button): added Spinner component

---
+++ src/components/feedback/Spinner/stories/Spinner.Default.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Spinner } from "./../index";
+
+import { size, appearance, isTransparent } from "./props";
+
+const story = {
+  title: "feedback/Spinner/Default",
+  components: [Spinner],
+  decorators: [
+    (Story) => (
+      <div style={{ margin: "3em" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const Default = (args) => <Spinner {...args} />;
+Default.args = {
+  size: "medium",
+  appearance: "blue",
+  isTransparent: false,
+};
+Default.argTypes = {
+  size,
+  appearance,
+  isTransparent,
+};
+
+export default story;
