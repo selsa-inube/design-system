8a41e81dd96e84558b75497d17e2e45b7e5744ec
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-08 17:34:37 -0500

    feat(exportComponents): added export for the components button, spinner and the hook usemediaquery

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ export * from "./Label";
 export * from "./Switch";
 export * from "./Stack";
 export * from "./Blanket";
+export * from "./Spinner";
+export * from "./Button";
+export { useMediaQuery } from "./../useMediaQuery";
