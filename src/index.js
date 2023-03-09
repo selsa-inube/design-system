3d5c533b4496d008ae6649d30a653caed769c188
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-09 17:34:48 -0500

    fix(avatar): added the export of the avatar-component to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+// inputs
+
 export * from "./Label";
 export * from "./Switch";
+
+// layouts
+
 export * from "./Stack";
+
+// utils
 export * from "./Blanket";
+
+//data
+
+export * from "./Avatar";
