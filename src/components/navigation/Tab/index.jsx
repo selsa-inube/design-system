000ecef4ff10bf48ab978f339a5c3d3901027cfa
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-23 12:21:44 -0500

    fix(tab): change the data-type for label

--- src/components/navigation/Tab/index.jsx
+++ src/components/navigation/Tab/index.jsx
@@ -32,7 +32,7 @@ Tab.propTypes = {
   isSelected: PropTypes.bool,
   id: PropTypes.string,
   handleClick: PropTypes.func.isRequired,
-  label: PropTypes.node.isRequired,
+  label: PropTypes.string.isRequired,
 };
 
 export { Tab };
