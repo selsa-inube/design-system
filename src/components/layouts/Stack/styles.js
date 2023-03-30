799ac456c0f1a7d011c69301430978e59c2e69bd
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-03-30 11:45:35 -0500

    fix(stack): fixed the issue related with the spacing(width-property)

--- src/components/layouts/Stack/styles.js
+++ src/components/layouts/Stack/styles.js
@@ -7,6 +7,8 @@ const StyledFlex = styled.div`
   flex-direction: ${({ direction }) => direction};
   flex-wrap: ${({ wrap }) => wrap};
   gap: ${({ gap }) => gap};
+  width: 100%;
+  height: 100%;
 `;
 
 export { StyledFlex };
