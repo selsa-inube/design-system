43f4ddead9fefb35eb839676ec1f59db32b48a70
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-04-14 13:21:24 -0500

    fix(breadcrumbMenu): remove unnecessary file of props

--- src/components/navigation/BreadcrumbMenu/stories/BreadcrumbMenu.Default.stories.js
+++ src/components/navigation/BreadcrumbMenu/stories/BreadcrumbMenu.Default.stories.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BreadcrumbMenu } from "../index";
 import { BrowserRouter } from "react-router-dom";
-import { routes } from "./props";
 
 const story = {
   title: "navigation/BreadcrumbMenu/Default",
@@ -34,7 +33,18 @@ Default.args = {
   ],
 };
 Default.argTypes = {
-  routes,
+  routes: {
+    description:
+      "The breadcrumb-component will utilize this routes for display, and consequently, it can be employed to locate the source of breadcrumbLinks present within this component.",
+    table: {
+      defaultValue: {
+        summary: `This structure must be present [{
+           label: "",
+          path: "",
+          id: ""}]`,
+      },
+    },
+  },
 };
 
 export default story;
