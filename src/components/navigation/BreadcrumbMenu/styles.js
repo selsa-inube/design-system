df4f895144ec50845840dade98b581c786ce8064
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-04-18 15:54:38 -0500

    fix(breadcrumbMenu): adjust the borderRadius-property

--- src/components/navigation/BreadcrumbMenu/styles.js
+++ src/components/navigation/BreadcrumbMenu/styles.js
@@ -4,7 +4,7 @@ import { colors } from "../../../shared/colors/colors";
 const StyledBreadcrumbMenu = styled.div`
   max-width: 160px;
   box-shadow: 0px 2px 4px ${colors.ref.palette.neutralAlpha.n50A};
-  border-radius: 5px;
+  border-radius: 4px;
 `;
 
 export { StyledBreadcrumbMenu };
