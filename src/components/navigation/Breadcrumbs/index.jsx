4fc6079672bcc4198993a999dcd611bc6528dade
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-04-25 14:21:56 -0500

    fix(breadcrumbs): added the use of Breadcrumb Ellipsis inside of breadcrumbs to get shortened paths

--- src/components/navigation/Breadcrumbs/index.jsx
+++ src/components/navigation/Breadcrumbs/index.jsx
@@ -4,34 +4,77 @@ import PropTypes from "prop-types";
 import { StyledBreadcrumbs } from "./styles";
 
 import { BreadcrumbLink } from "../../navigation/BreadcrumbLink";
+import { useMediaQuery } from "../../../hooks/useMediaQuery";
+import { BreadcrumbEllipsis, typos } from "../../navigation/BreadcrumbEllipsis";
 
 const Breadcrumbs = (props) => {
   const { route } = props;
 
   const crumbs = route.split("/").filter((crumb) => crumb !== "");
+  crumbs.unshift("Home");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const maxCrumbs = isDesktop ? 5 : 3;
+  let breadcrumbItems = [];
 
-  const breadcrumbItems = crumbs.map((crumb, index) => ({
-    path: `/${crumbs.slice(0, index + 1).join("/")}`,
-    crumb: `${crumb.charAt(0).toUpperCase() + crumb.slice(1)}`,
-    isActive: index === crumbs.length - 1,
+  if (crumbs.length > maxCrumbs) {
+    const firstCrumb = {
+      path: `/${crumbs[0]}`,
+      crumb: `${crumbs[0].charAt(0).toUpperCase() + crumbs[0].slice(1)}`,
+      isActive: false,
+    };
+
+    const lastCrumb = {
+      path: `/${crumbs.slice(-1)[0]}`,
+      crumb: `${
+        crumbs.slice(-1)[0].charAt(0).toUpperCase() +
+        crumbs.slice(-1)[0].slice(1)
+      }`,
+      isActive: true,
+    };
+
+    const middleCrumb = {
+      path: "",
+      crumb: "",
+      isActive: false,
+    };
+
+    breadcrumbItems.push(firstCrumb, middleCrumb, lastCrumb);
+  } else {
+    breadcrumbItems = crumbs.map((crumb, index) => ({
+      path: `/${crumbs.slice(0, index + 1).join("/")}`,
+      crumb: `${crumb.charAt(0).toUpperCase() + crumb.slice(1)}`,
+      isActive: index === crumbs.length - 1,
+    }));
+  }
+
+  const routesForEllipsis = crumbs.slice(1, -1).map((item) => ({
+    label: item,
+    path: `/${item.toLowerCase()}`,
+    id: item,
   }));
 
   return (
     <StyledBreadcrumbs>
-      <BreadcrumbLink key="home" path="/" id="Home" label="Home" />
-      {breadcrumbItems.map(({ path, crumb, isActive }) => (
-        <BreadcrumbLink
-          key={path}
-          path={path}
-          id={path}
-          label={crumb}
-          isActive={isActive}
-        />
-      ))}
+      {breadcrumbItems.map(({ path, crumb, isActive }, index) =>
+        index === 1 && crumbs.length > maxCrumbs ? (
+          <BreadcrumbEllipsis
+            key={`breadcrumb-ellipsis-${index}`}
+            typo={typos[isDesktop ? "labelLarge" : "labelSmall"]}
+            routes={routesForEllipsis}
+          />
+        ) : (
+          <BreadcrumbLink
+            key={path}
+            path={path}
+            id={path}
+            label={crumb}
+            isActive={isActive}
+          />
+        )
+      )}
     </StyledBreadcrumbs>
   );
 };
-
 Breadcrumbs.propTypes = {
   route: PropTypes.string.isRequired,
 };
--- src/components/navigation/Breadcrumbs/styles.js
+++ src/components/navigation/Breadcrumbs/styles.js
@@ -3,9 +3,12 @@ import styled from "styled-components";
 const StyledBreadcrumbs = styled.ul`
   padding: 0;
   margin: 0;
-  & > *:not(:last-child)::after {
+  & > li:not(:last-child)::after {
     content: "/";
     margin: 0 8px;
+  }
+  & li > label {
+    display: inherit;
   }
 `;
 
