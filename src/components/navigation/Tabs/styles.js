89d047d256dd3559c188a49525b20ca7af75f579
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-04-25 14:02:05 -0500

    fix(Tabs): adjust for component overflow and units of measure in the css

--- src/components/navigation/Tabs/styles.js
+++ src/components/navigation/Tabs/styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import { colors } from "../../../shared/colors/colors";
 
 const StyledTabs = styled.div`
+  box-sizing: border-box;
   overflow-x: auto;
   white-space: nowrap;
   width: 100%;
   border-bottom: 2px solid ${colors.ref.palette.neutral.n40};
-  padding: 0rem 1.5rem;
+  padding: 0px 16px;
 `;
 
 export { StyledTabs };
