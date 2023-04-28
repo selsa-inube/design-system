cf35a1714ef7c100b61e4a2fd89f456669948684
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-04-28 13:21:47 -0500

    fix(useMediaQueries): change the way to make validations by uses try-catch block to call a function

--- src/hooks/useMediaQueries.js
+++ src/hooks/useMediaQueries.js
@@ -1,57 +1,60 @@
 import { useEffect, useState } from "react";
 
-const useMediaQueries = (initialQueries) => {
-  if (!Array.isArray(initialQueries)) {
-    throw new Error("Invalid parameter: queries must be an array");
+const validateQueries = (queries) => {
+  if (!Array.isArray(queries)) {
+    return new Error("Invalid parameter: queries must be an array");
   }
 
-  const mediaQueryList = initialQueries.map((query) => {
-    if (typeof query !== "string" || query.trim() === "") {
-      throw new Error("Invalid query: must be a non-empty string");
-    }
-    return window.matchMedia(query);
+  const invalidQuery = queries.find(
+    (query) => typeof query !== "string" || query.trim() === ""
+  );
+  if (invalidQuery) {
+    return new Error("Invalid query: must be a non-empty string");
+  }
+};
+
+const initializeState = (mediaQueryList) => {
+  const initialState = {};
+  mediaQueryList.forEach((query) => {
+    initialState[query.media] = query.matches;
   });
+  return initialState;
+};
 
-  const initializeState = () => {
-    const initialState = {};
-    mediaQueryList.forEach((query) => {
-      initialState[query.media] = query.matches;
-    });
-    return initialState;
-  };
-
-  const [matches, setMatches] = useState(initializeState);
-
-  const handleChange = (event) => {
-    setMatches((prevState) => {
-      if (prevState[event.media] === event.matches) {
-        // No need to update state if the value hasn't changed
-        return prevState;
-      }
-      return { ...prevState, [event.media]: event.matches };
-    });
-  };
+const useMediaQueries = (initialQueries) => {
+  const [matches, setMatches] = useState({});
 
   useEffect(() => {
-    if (initialQueries.length > 0) {
-      const handleInitialQueries = () => {
-        setMatches(initializeState());
-      };
-
-      handleInitialQueries();
-
-      mediaQueryList.forEach((mediaQueryObject) =>
-        mediaQueryObject.addEventListener("change", handleChange)
-      );
-
-      return () =>
-        mediaQueryList.forEach((mediaQueryObject) =>
-          mediaQueryObject.removeEventListener("change", handleChange)
-        );
+    const validationError = validateQueries(initialQueries);
+    if (validationError) {
+      throw validationError;
     }
+
+    const mediaQueryList = initialQueries.map((query) =>
+      window.matchMedia(query)
+    );
+
+    setMatches(initializeState(mediaQueryList));
+
+    const handleChange = (event) => {
+      setMatches((prevState) => {
+        if (prevState[event.media] === event.matches) {
+          return prevState;
+        }
+        return { ...prevState, [event.media]: event.matches };
+      });
+    };
+
+    mediaQueryList.forEach((mediaQueryObject) =>
+      mediaQueryObject.addEventListener("change", handleChange)
+    );
+
+    return () =>
+      mediaQueryList.forEach((mediaQueryObject) =>
+        mediaQueryObject.removeEventListener("change", handleChange)
+      );
   }, [initialQueries]);
 
   return matches;
 };
-
 export { useMediaQueries };
