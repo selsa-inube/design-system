496948ec29b7e7f3d28020383afe6b55379de7fa
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-03 13:39:03 -0500

    fix: removed exports that did not go into the .storybook/mian.js file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 import { mergeConfig } from "vite";
-import react from "@vitejs/plugin-react";
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
