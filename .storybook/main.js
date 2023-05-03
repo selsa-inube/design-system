ba8c30e1e0bfe5bc20486c5a8e54197467101e59
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-03 12:45:30 -0500

    chore: update Storybook to latest version and integrate with Vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,16 @@
-module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-a11y",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+import { mergeConfig } from "vite";
+export default {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  features: {
-    interactionsDebugger: true,
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ["storybook-addon-designs"],
+      },
+    });
   },
 };
