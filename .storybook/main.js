8a31ef32ec0366b7f291c76b24553608a4449527
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-03 13:14:27 -0500

    fix(SkeletonLine.stories): threw the story extension to jsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 import { mergeConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
