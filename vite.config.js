b3a1490d0e6b9f0425d89637b5b905da6375c2bf
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-05 10:05:12 -0500

    fix: fixed the module import bug, which caused it to break where the library was used

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: "build",
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "design-system",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+      exports: "named",
     },
-    plugins: [react()],
-  };
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
 });
