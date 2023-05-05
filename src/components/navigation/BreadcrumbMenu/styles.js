8b3ca2809e57189f6bf802d998b617105eb209b5
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-05-05 18:42:14 -0500

    fix(breadCrumbMenu): align menu to the left of the ellipsis

--- src/components/navigation/BreadcrumbMenu/styles.js
+++ src/components/navigation/BreadcrumbMenu/styles.js
@@ -3,6 +3,7 @@ import { colors } from "../../../shared/colors/colors";
 
 const StyledBreadcrumbMenu = styled.div`
   position: absolute;
+  left: 12%;
   max-width: 160px;
   box-shadow: 0px 2px 4px ${colors.ref.palette.neutralAlpha.n50A};
   border-radius: 4px;
