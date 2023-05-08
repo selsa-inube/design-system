dc48b56657fa1a3f637535c7d9765c89cc7fb583
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-08 17:47:46 -0500

    fix(stories.main): deregistered the addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,7 @@
 import { mergeConfig } from "vite";
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "storybook-addon-designs",
-  ],
+  addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
