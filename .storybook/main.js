bc0cd74a53870af84c0035de77154e036dca18c5
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-05-09 15:09:44 -0500

    fix(migrationVite): remove unnecessary addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,8 @@
-import { mergeConfig } from "vite";
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
+  addons: ["@storybook/addon-essentials"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      optimizeDeps: {
-        include: ["storybook-addon-designs"],
-      },
-    });
   },
 };
