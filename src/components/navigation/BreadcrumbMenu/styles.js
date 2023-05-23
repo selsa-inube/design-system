a938377c6e6760e5f910dfbd513526a62d5984f7
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-05-23 14:55:13 -0500

    fix(breadCrumbs): adjust the width property

--- src/components/navigation/BreadcrumbMenu/styles.js
+++ src/components/navigation/BreadcrumbMenu/styles.js
@@ -3,6 +3,7 @@ import { colors } from "../../../shared/colors/colors";
 
 const StyledBreadcrumbMenu = styled.div`
   position: absolute;
+  width: fit-content;
   max-width: 160px;
   min-width: 100px;
   box-shadow: 0px 2px 4px ${colors.ref.palette.neutralAlpha.n50A};
