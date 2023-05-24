a01f01248de8614d0745973e2fc87a7e2c4be61c
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-05-24 12:36:23 -0500

    feat(validateCssSpacing): notifies the user when an error is triggered

--- src/utilities/validateCssSpacing.js
+++ src/utilities/validateCssSpacing.js
@@ -6,17 +6,29 @@ const globalValuesPropertiesCss = ["inherit", "initial", "unset", "auto"];
 const validateCssSpacing = (size) => {
   const splitPixels = size.trim().split(" ");
 
-  if (size === "" || splitPixels.length === 0 || splitPixels.length > 4) {
-    return false;
+  if (size === "") {
+    return console.warn(
+      "if the input argument is not passed, the component will take the default value"
+    );
   }
 
-  const isGlobalCssValue = globalValuesPropertiesCss.includes(size);
-
-  if (isGlobalCssValue && splitPixels.length === 1) {
-    return true;
+  if (splitPixels.length === 0 || splitPixels.length > 4) {
+    return console.error("The provided CSS spacing size is invalid. ");
   }
 
-  return splitPixels.every((valueSize) => regex.test(valueSize));
+  if (globalValuesPropertiesCss.includes(size) && splitPixels.length === 1) {
+    return console.error(
+      "Provide a single valid spacing measurement in the correct format, following CSS syntax"
+    );
+  }
+
+  if (!splitPixels.every((valueSize) => regex.test(valueSize))) {
+    console.error(
+      "It must be a number followed by 'px', taking into account the CSS format"
+    );
+  }
+
+  return true;
 };
 
 export { validateCssSpacing };
