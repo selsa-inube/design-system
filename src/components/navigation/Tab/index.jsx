c3f0f69a688ce15ccdc80677412477914d6bb5cf
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-06-02 16:12:33 -0500

    fix(Tab): eliminate unnecessary imports

--- src/components/navigation/Tab/index.jsx
+++ src/components/navigation/Tab/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Label } from "../../inputs/Label";
-
 import { Text } from "../../data/Text";
 
 import { StyledTab } from "./styles";
