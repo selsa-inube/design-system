8687dcee2515ce89a615d78ed87cc3d1c9d8657b
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-06-21 09:36:38 -0500

    fix(table): added modal to open in responsive mode

---
+++ src/components/data/Table/DisplayEntry/index.jsx
@@ -0,0 +1,30 @@
+import { InteractiveModal } from "@components/feedback/InteractiveModal";
+import { useState } from "react";
+import { MdOpenInNew } from "react-icons/md";
+
+function DisplayEntry(props) {
+  const { entry, actions, title, titleLabels } = props;
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <>
+      <MdOpenInNew onClick={handleToggleModal} />
+      {showModal && (
+        <InteractiveModal
+          title={title}
+          closeModal={handleToggleModal}
+          infoData={entry}
+          actions={actions}
+          labels={titleLabels}
+        />
+      )}
+    </>
+  );
+}
+
+export { DisplayEntry };
