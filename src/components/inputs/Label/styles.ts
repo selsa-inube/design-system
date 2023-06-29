db4e7b9779aa059d2cdab354e7bede90559e497b
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-06-29 14:40:31 -0500

    refactor(Lable): adjust the ident and formatting of the

--- src/components/inputs/Label/styles.ts
+++ src/components/inputs/Label/styles.ts
@@ -31,17 +31,6 @@ const getColor = (
 
 console.log();
 
-/* const StyledLabel = styled.label<LabelProps>`
-  font-family: ${typography.sys.typescale.labelLarge.font};
-  font-size: ${({ typo } ) => typography.sys.typescale[typo].size};
-  font-weight: ${({ typo }) => typography.sys.typescale[typo].weight};
-  letter-spacing: ${({ typo }) => typography.sys.typescale[typo].tracking};
-  line-height: ${({ typo }) => typography.sys.typescale[typo].lineHeight};
-  color: ${({ isDisabled, isFocused, isInvalid }) =>
-    getColor(isDisabled, isFocused, isInvalid)};
-`;
- */
-
 const StyledLabel = styled.label`
   font-family: ${typography.sys.typescale.labelLarge.font};
   font-size: ${({ typo }: LabelProps) =>
