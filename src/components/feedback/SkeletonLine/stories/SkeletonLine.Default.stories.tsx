3b64ccbbc8c24210505d3f7f38884dce80917003
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-04 15:15:36 -0500

    feat(skeletonLine): migrate <SkeletonLine /> to TypeScript

--- src/components/feedback/SkeletonLine/stories/SkeletonLine.Default.stories.tsx
+++ src/components/feedback/SkeletonLine/stories/SkeletonLine.Default.stories.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 import { SkeletonLine } from "..";
 import { parameters, width, isAnimated } from "./props";
+import { ISkeletonLineProps } from "../interfaces/SkeletonLine.interface";
 
 const story = {
   title: "feedback/SkeletonLine/Default",
   components: [SkeletonLine],
   parameters,
   decorators: [
-    (Story) => (
+    (Story: React.ElementType) => (
       <div style={{ margin: "3em" }}>
         <Story />
       </div>
     ),
   ],
 };
-const Default = (args) => <SkeletonLine {...args} />;
+const Default = (args: ISkeletonLineProps) => <SkeletonLine {...args} />;
 Default.args = {
   width: "100%",
   isAnimated: false,
