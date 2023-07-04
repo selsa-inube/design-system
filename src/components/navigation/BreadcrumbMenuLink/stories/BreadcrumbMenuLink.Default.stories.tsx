0773fa99219b2bfb19b224be9d55dc5b7ecddfef
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-04 17:23:51 -0500

    feat(breadcrumbMenuLink): migrate <BreadcrumbMenuLink /> to TypeScript

--- src/components/navigation/BreadcrumbMenuLink/stories/BreadcrumbMenuLink.Default.stories.tsx
+++ src/components/navigation/BreadcrumbMenuLink/stories/BreadcrumbMenuLink.Default.stories.tsx
@@ -3,12 +3,13 @@ import { BreadcrumbMenuLink } from "../index";
 import { BrowserRouter } from "react-router-dom";
 
 import { id, label, typo, path } from "./props";
+import { IBreadcrumbMenuLinkProps } from "../interfaces/BreadcrumbMenuLink.interface";
 
 const story = {
   title: "navigation/BreadcrumbMenuLink/Default",
   components: [BreadcrumbMenuLink],
   decorators: [
-    (Story) => (
+    (Story: React.ElementType) => (
       <div style={{ margin: "3em" }}>
         <BrowserRouter>
           <Story />
@@ -18,7 +19,9 @@ const story = {
   ],
 };
 
-export const Default = (args) => <BreadcrumbMenuLink {...args} />;
+export const Default = (args: IBreadcrumbMenuLinkProps) => (
+  <BreadcrumbMenuLink {...args} />
+);
 Default.args = {
   label: "Privileges",
   path: "/privileges",
