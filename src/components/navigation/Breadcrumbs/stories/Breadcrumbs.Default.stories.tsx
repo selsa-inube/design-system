f4dc9a9326fc70cfeac12ae4a7e89823a93df817
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-06 12:07:57 -0500

    feat(breadcrumbs): migrate <Breadcrumbs /> to TypeScript

--- src/components/navigation/Breadcrumbs/stories/Breadcrumbs.Default.stories.tsx
+++ src/components/navigation/Breadcrumbs/stories/Breadcrumbs.Default.stories.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 import { Breadcrumbs } from "../index";
 import { BrowserRouter } from "react-router-dom";
 import { route } from "./props";
+import { IBreadcrumbsProps } from "../interfaces/Breadcrumbs.interface";
 
 const story = {
   title: "navigation/Breadcrumbs/Default",
   components: [Breadcrumbs],
   decorators: [
-    (Story) => (
+    (Story: React.ElementType) => (
       <div style={{ margin: "3em" }}>
         <BrowserRouter>
           <Story />
@@ -17,7 +17,7 @@ const story = {
   ],
 };
 
-export const Default = (args) => <Breadcrumbs {...args} />;
+export const Default = (args: IBreadcrumbsProps) => <Breadcrumbs {...args} />;
 Default.args = {
   route: "Privileges/Users",
 };
