4e306ae105fec05ff6ebb3a61b3264749956be09
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-10 10:58:04 -0500

    fix(table): change the type of displyEntry titles

--- src/components/data/Table/interface.tsx
+++ src/components/data/Table/interface.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { DisplayEntry } from "./DisplayEntry";
 import {
   StyledTable,
--- src/components/data/Table/interfaces/Table.DisplayEntry.interface.ts
+++ src/components/data/Table/interfaces/Table.DisplayEntry.interface.ts
@@ -1,12 +1,13 @@
 import { IAction } from "./Table.Action.interface";
 import { IEntry } from "./Table.Entry.interface";
+import { ITitle } from "./Table.Title.interface";
 
 export interface IDisplayEntryProps {
   portalId: string;
   entry: IEntry;
   actions: IAction[];
   title: string;
-  titleLabels: Array<string>;
+  titleLabels: ITitle[];
   infoTitle?: string;
   actionsTitle?: string;
 }
