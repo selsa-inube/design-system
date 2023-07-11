213ed5e6b73e7cdf8452dbede654e75d2e7b2f3d
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-11 12:05:41 -0500

    fix(blanket): remove unnecessary properties

--- src/components/utils/Blanket/index.tsx
+++ src/components/utils/Blanket/index.tsx
@@ -1,9 +1,16 @@
+import { Stack } from "@src/components/layouts/Stack";
 import { BlanketProps } from "./interfaces/interface.Blanket";
 import { StyledBlanket } from "./styles";
 
 const Blanket = (props: BlanketProps) => {
   const { children } = props;
-  return <StyledBlanket>{children}</StyledBlanket>;
+  return (
+    <StyledBlanket>
+      <Stack alignItems="center" justifyContent="center">
+        {children}
+      </Stack>
+    </StyledBlanket>
+  );
 };
 
 export { Blanket };
