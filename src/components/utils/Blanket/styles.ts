c3b2a3ecd970ff3bc8a6cfc1d6b3db950f0311ef
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-11 10:53:56 -0500

    fix(blanket): remove unnecessary properties that cause trouble

--- src/components/utils/Blanket/styles.ts
+++ src/components/utils/Blanket/styles.ts
@@ -2,18 +2,12 @@ import styled from "styled-components";
 import { colors } from "../../../shared/colors/colors";
 
 const StyledBlanket = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 0%;
-  bottom: 0%;
-  left: 0%;
-  right: 0%;
+  position: fixed;
+  inset: 0;
   background-color: ${colors.ref.palette.neutralAlpha.n100A};
   border: none;
   z-index: 1;
-  overflow: auto;
+  overflow-y: auto;
 `;
 
 export { StyledBlanket };
