530fa84f98568e0323517cf7652e9d49ceacb366
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-14 16:51:42 -0500

    fix(blanket): remove unnecessary code

--- src/components/utils/Blanket/index.tsx
+++ src/components/utils/Blanket/index.tsx
@@ -3,7 +3,6 @@ import { BlanketProps } from "./interfaces/interface.Blanket";
 import { StyledBlanket } from "./styles";
 import { useMediaQuery } from "@src/hooks/useMediaQuery";
 
-// just a test
 const Blanket = (props: BlanketProps) => {
   const { children } = props;
   const isSmallScreen = useMediaQuery("(max-width: 580px)");
