f6195a0330e5c776457a3d311574317e018b135f
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-07-14 15:12:45 -0500

    fix: fix errors filed in chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  core: {
+    disableTelemetry: true,
+  },
   addons: ["@storybook/addon-essentials"],
   docs: {
     autodocs: true,
