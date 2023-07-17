2bd3bfee54066e734d5ece0cc8940e0e12153d44
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-07-17 11:38:39 -0500

    fix(dropdownItem): remove unnecesary code

--- src/components/inputs/DropDownItem/styles.js
+++ src/components/inputs/DropDownItem/styles.js
@@ -3,9 +3,19 @@ import styled from "styled-components";
 import { colors } from "../../../shared/colors/colors";
 import { typography } from "../../../shared/typography/typography";
 
+const getColor = (isDisabled, isSelected) => {
+  if (isDisabled) {
+    return colors.ref.palette.neutral.n70;
+  }
+  if (isSelected && !isDisabled) {
+    return colors.sys.text.primary;
+  }
+  return colors.sys.text.dark;
+};
+
 const getBorderLeft = (isDisabled, isSelected) => {
   if (isSelected && !isDisabled) {
-    return `5px solid ${colors.ref.palette.neutral.n900}`;
+    return `5px solid ${colors.sys.actions.primary.filled}`;
   }
 
   return `0px`;
@@ -38,14 +48,12 @@ const StyledDropDownItem = styled.li`
   font-weight: ${typography.sys.typescale.bodyMedium.weight};
   line-height: ${typography.sys.typescale.bodyMedium.lineHeight};
   letter-spacing: ${typography.sys.typescale.bodyMedium.tracking};
-  color: ${({ isDisabled }) => isDisabled && colors.ref.palette.neutral.n70};
+  color: ${({ isDisabled, isSelected }) => getColor(isDisabled, isSelected)};
   cursor: ${({ isDisabled }) => (!isDisabled ? "pointer" : "not-allowed")};
-  pointer-events: ${({ isDisabled }) => isDisabled && "none"};
   border-left: ${({ isDisabled, isSelected }) =>
     getBorderLeft(isDisabled, isSelected)};
   background-color: ${({ isDisabled, isSelected }) =>
     getBackgroundColor(isDisabled, isSelected)};
-
   &:hover {
     background-color: ${colors.ref.palette.neutral.n30};
   }
