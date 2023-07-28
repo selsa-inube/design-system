9b2431962c519b86239529e0183a47b0378beebb
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-07-28 16:37:04 -0500

    fix(Icon.interface): remove theme of the interface

--- src/components/data/Icon/interfaces/Icon.interface.ts
+++ src/components/data/Icon/interfaces/Icon.interface.ts
@@ -2,7 +2,6 @@ import { Appearance } from "@src/shared/types/Appearance.type";
 import { Shape } from "../types/Icon.Shape.type";
 import { Spacing } from "../types/Icon.Spacing.type";
 import { Variant } from "../types/Icon.Variant.type";
-import { inube } from "@src/shared/tokens";
 
 export interface IIconProps {
   appearance: Appearance;
@@ -14,6 +13,5 @@ export interface IIconProps {
   variant?: Variant;
   shape?: Shape;
   size?: string;
-  theme?: typeof inube;
   handleClick?: () => void;
 }
