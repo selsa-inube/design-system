6c63a31d34eb0660e407902d1e97ed1cec98dfec
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-07-31 14:10:38 -0500

    refactor(Stack): remove the interface to index file

--- src/components/layouts/Stack/index.tsx
+++ src/components/layouts/Stack/index.tsx
@@ -1,7 +1,25 @@
 import { transformedMeasure } from "@src/utilities/transformedMeasure";
-import { IStackProps } from "./interfaces/Stack.interface";
 
 import { StyledFlex } from "./styles";
+import { WrapControl } from "./types/Stack.WrapControl.type";
+import { DirectionAlignment } from "./types/Stack.DirectionAlignment.type";
+import { JustifyContent } from "./types/Stack.JustifyContent.type";
+import { AlignItem } from "./types/Stack.AlignItems.type";
+import { AlignContent } from "./types/Stack.AlignContent.type";
+
+export interface IStackProps {
+  children?: React.ReactNode;
+  wrap?: WrapControl;
+  direction?: DirectionAlignment;
+  justifyContent?: JustifyContent;
+  alignItems?: AlignItem;
+  alignContent?: AlignContent;
+  height?: string;
+  width?: string;
+  gap?: string;
+  margin?: string;
+  padding?: string;
+}
 
 const Stack = (props: IStackProps) => {
   const {
