aea1d08124ba1972a82c8efb6097415c8aaa55f8
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-08-01 16:16:05 -0500

    refactor(Icon.type.Theme): the name of the type ITheme is changed to standard Theme

--- src/components/data/Icon/interfaces/Icon.interface.ts
+++ src/components/data/Icon/interfaces/Icon.interface.ts
@@ -2,9 +2,9 @@ import { Appearance } from "@src/shared/types/Appearance.type";
 import { Shape } from "../types/Icon.Shape.type";
 import { Spacing } from "../types/Icon.Spacing.type";
 import { Variant } from "../types/Icon.Variant.type";
-import { IThemed } from "../types/Icon.Theme";
+import { Themed } from "../types/Icon.Theme";
 
-export interface IIconProps extends IThemed {
+export interface IIconProps extends Themed {
   appearance: Appearance;
   cursorHover?: boolean;
   parentHover?: boolean;
--- src/components/data/Icon/types/Icon.Theme.ts
+++ src/components/data/Icon/types/Icon.Theme.ts
@@ -1,3 +1,3 @@
 import { inube } from "@src/shared/tokens";
 
-export type IThemed = { theme: typeof inube };
+export type Themed = { theme: typeof inube };
