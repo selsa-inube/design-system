ed3222fb8d0b61713f5b5d32e79b2424b71718a1
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-08-01 17:00:47 -0500

    fix(Icon): adjust imports

--- src/components/data/Icon/stories/Icon.stories.tsx
+++ src/components/data/Icon/stories/Icon.stories.tsx
@@ -1,10 +1,10 @@
+import { ThemeProvider } from "styled-components";
 import { MdAdb } from "react-icons/md";
 
 import { Icon, IIconProps } from "..";
 
 import { props } from "../props";
 import { presente } from "@src/shared/themes/presente";
-import { ThemeProvider } from "styled-components";
 
 const story = {
   title: "data/Icon",
