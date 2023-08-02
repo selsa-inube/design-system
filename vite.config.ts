929e550d478c9e9848e418b59b979a915f48ce4c
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-08-02 15:31:32 -0500

    fix(aliases): create aliases for main folders in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { resolve } from "path";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitesconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@data": resolve(__dirname, "src/data"),
+      "@feedback": resolve(__dirname, "src/feedback"),
+      "@inputs": resolve(__dirname, "src/inputs"),
+      "@layouts": resolve(__dirname, "src/layouts"),
+      "@navigation": resolve(__dirname, "src/navigation"),
+      "@utils": resolve(__dirname, "src/utils"),
+      "@hooks": resolve(__dirname, "src/hooks"),
+      "@shared": resolve(__dirname, "src/shared"),
+      "@utilities": resolve(__dirname, "src/utilities"),
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
