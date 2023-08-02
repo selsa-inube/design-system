44dd82100ed7eed439b0d32efed2b17bd461ff2c
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-08-01 19:48:30 -0500

    fix(types): merge to main

--- src/shared/types/Appearance.type.ts
+++ src/shared/types/Appearance.type.ts
@@ -1,4 +1,4 @@
 import { inube } from "@src/shared/tokens";
 
-export const appearances = typeof inube.color.text;
+export const appearances = inube.color.text;
 export type Appearance = keyof typeof appearances;
