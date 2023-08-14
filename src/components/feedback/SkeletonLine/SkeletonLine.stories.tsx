717808cd95b5bbc52414129c537525d37c1735c0
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-08-14 12:36:49 -0500

    feat(skeletonLine): create Default and Themed stories

--- src/components/feedback/SkeletonLine/SkeletonLine.stories.tsx
+++ src/components/feedback/SkeletonLine/SkeletonLine.stories.tsx
@@ -1,5 +1,7 @@
 import { SkeletonLine, ISkeletonLineProps } from ".";
 import { props, parameters } from "./props";
+import { ThemeProvider } from "styled-components";
+import { presente } from "@shared/themes/presente";
 
 const story = {
   title: "feedback/SkeletonLine",
@@ -12,6 +14,18 @@ Default.args = {
   width: "100%",
   animated: false,
 };
+const theme = {
+  ...presente,
+};
 
+export const Themed = (args: ISkeletonLineProps) => (
+  <ThemeProvider theme={theme}>
+    <SkeletonLine {...args} />
+  </ThemeProvider>
+);
+Themed.args = {
+  size: "100%",
+  animated: false,
+};
 export default story;
 export { Default };
