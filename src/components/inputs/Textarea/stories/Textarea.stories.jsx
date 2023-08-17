14e42ac07d0b31c21d153084f81d52fa2257e0bb
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-08-17 15:09:36 -0500

    feat(Textarea.stories): add story directory

---
+++ src/components/inputs/Textarea/stories/Textarea.stories.jsx
@@ -0,0 +1,29 @@
+import { Textarea } from "..";
+import { TextareaController } from "./Textarea.controller";
+
+import { props, parameters } from "../props";
+
+const story = {
+  title: "inputs/Textarea",
+  components: [Textarea],
+  parameters,
+  argTypes: props,
+};
+
+const Default = (args) => <TextareaController {...args} />;
+Default.args = {
+  label: "Textarea",
+  name: "textarea",
+  id: "textarea",
+  state: "pending",
+  placeholder: "Storybook Textarea",
+  isDisabled: false,
+  counter: true,
+  lengthThreshold: 20,
+  isRequired: true,
+  value:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam, reiciendis ipsum itaque unde odio voluptatum ab cumque deleniti dolore magnam quas hic rem, mollitia adipisci. Officiis accusamus aut consectetur",
+};
+
+export default story;
+export { Default };
