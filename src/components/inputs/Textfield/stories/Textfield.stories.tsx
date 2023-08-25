b977c166c45877454ac18c59be7d711bb829e812
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-08-25 18:44:17 -0500

    feat(TextField): implement only one history file

--- src/components/inputs/Textfield/stories/Textfield.stories.tsx
+++ src/components/inputs/Textfield/stories/Textfield.stories.tsx
@@ -1,7 +1,9 @@
+import { ThemeProvider } from "styled-components";
 import { Textfield, ITextfieldProps } from "..";
 import { TextfieldController } from "./TextfieldController";
 
 import { props, parameters } from "../props";
+import { presente } from "@src/shared/themes/presente";
 
 const story = {
   title: "inputs/Textfield",
@@ -21,8 +23,27 @@ Default.args = {
   validMessage: "The field has been successfully validated",
   size: "wide",
   readOnly: false,
+  disabled: false,
+  required: false,
+  type: "text",
+  errorMessages: "field cannot be empty, special characters cannot be used",
+  validMessages: "field has been successfully validated",
+};
+
+const theme = {
+  ...presente,
+};
+
+const Themed = (args: ITextfieldProps) => (
+  <ThemeProvider theme={theme}>
+    <TextfieldController {...args} />
+  </ThemeProvider>
+);
+
+Themed.args = {
+  ...Default.args,
 };
 
 export default story;
 
-export { Default };
+export { Default, Themed };
