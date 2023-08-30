b486cc594b34747e7bac2d1671408dcf7d22ce8c
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-08-30 15:58:29 -0500

    refactor(textarea): use new typography tokens

--- src/components/inputs/Textarea/styles.ts
+++ src/components/inputs/Textarea/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { colors } from "@shared/colors/colors";
-import { typography } from "@shared/typography/typography";
 import { Status } from "./props";
 import { ITextareaProps } from ".";
+import { inube } from "@shared/tokens";
 
 const getGrid = (label: string | undefined, counter: boolean | undefined) => {
   if (label && counter) {
@@ -75,10 +75,11 @@ const StyledContainerLabel = styled.div`
 const StyledTextarea = styled.textarea`
   border-radius: 8px;
   padding: 8px 12px 8px 16px;
-  font-family: ${typography.sys.typescale.bodyLarge.font};
-  font-size: ${typography.sys.typescale.bodyLarge.size};
-  font-weight: ${typography.sys.typescale.bodyLarge.weight};
-  line-height: ${typography.sys.typescale.bodyLarge.lineHeight};
+  font-family: ${inube.typography.body.large.font};
+  font-size: ${inube.typography.body.large.size};
+  font-weight: ${inube.typography.body.large.weight};
+  line-height: ${inube.typography.body.large.lineHeight};
+  letter-spacing: ${inube.typography.body.large.tracking};
   width: ${({ fullwidth }: ITextareaProps) =>
     fullwidth ? "calc(100% - 32px)" : "452px"};
   height: 120px;
