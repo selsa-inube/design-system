023b8c86c297c496d7013d84d5e5c1423838f49e
Andres <74839801+Andresbl123@users.noreply.github.com>
2023-08-31 11:38:33 -0500

    "refactor(Textarea): remove unnecessary spacings"

--- src/components/inputs/Textarea/styles.ts
+++ src/components/inputs/Textarea/styles.ts
@@ -10,8 +10,7 @@ const StyledContainer = styled.div`
 
 const StyledTextarea = styled.textarea`
   border-radius: 8px;
-  padding: ${inube.spacing.s100} ${inube.spacing.s150} ${inube.spacing.s100}
-    ${inube.spacing.s200};
+  padding: ${inube.spacing.s100} ${inube.spacing.s150} ${inube.spacing.s100} ${inube.spacing.s200};
   font-family: ${inube.typography.body.large.font};
   font-size: ${inube.typography.body.large.size};
   font-weight: ${inube.typography.body.large.weight};
