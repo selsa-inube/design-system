2965b9feb8b1590d0e8edc2c5de37e4a6e3abf6e
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-09-05 11:07:20 -0500

    feat(Tab.stiles): refactor to use new color tokens

--- src/components/navigation/Tab/styles.ts
+++ src/components/navigation/Tab/styles.ts
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
+import { inube } from "@shared/tokens";
 import { ITabProps } from ".";
-import { colors } from "@shared/colors/colors";
 
 const StyledTab = styled.li`
   width: fit-content;
   user-select: none;
   list-style-type: none;
-  border-bottom: ${({ isSelected, isDisabled }: ITabProps) =>
+  border-bottom: ${({ isSelected, isDisabled, theme }: ITabProps) =>
     isSelected &&
     !isDisabled &&
-    `4px solid ${colors.sys.actions.primary.filled}`};
+    `4px solid ${
+      theme?.color.stroke.primary.regular || inube.color.stroke.primary.regular
+    }`};
 
   & > p {
     cursor: ${({ isDisabled }: ITabProps) =>
