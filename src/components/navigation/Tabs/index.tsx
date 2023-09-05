71fca6fafc50204878ff3d1dcc49ec53886f9331
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-09-05 10:21:26 -0500

    refactor(Tab): rename isDisabled prop for just disabled

--- src/components/navigation/Tabs/index.tsx
+++ src/components/navigation/Tabs/index.tsx
@@ -64,7 +64,7 @@ const Tabs = ({
             </StyledIconWrapper>
             <Tab
               key={selectedTab}
-              isDisabled={transformedIsDisabled}
+              disabled={transformedIsDisabled}
               isSelected={true}
               id={selectedTab}
               handleClick={() => handleSelectedTab(selectedTab)}
@@ -90,7 +90,7 @@ const Tabs = ({
         {tabs.map((tab) => (
           <Tab
             key={tab.id}
-            isDisabled={tab.isDisabled}
+            disabled={tab.isDisabled}
             isSelected={tab.id === selectedTab}
             id={tab.id}
             handleClick={() => handleSelectedTab(tab.id)}
