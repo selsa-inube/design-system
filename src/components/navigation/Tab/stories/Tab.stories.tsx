a77ea28fff834595383b1f6a98bb986331942446
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-09-05 11:40:11 -0500

    feat(Tab.stories): refactor to use new color tokens

--- src/components/navigation/Tab/stories/Tab.stories.tsx
+++ src/components/navigation/Tab/stories/Tab.stories.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from "styled-components";
 import { Tab, ITabProps } from "../index";
 import { TabController } from "./TabController";
 
 import { props } from "../props";
+
+import { presente } from "@src/shared/themes/presente";
 
 const story = {
   title: "navigation/Tab",
@@ -9,7 +12,7 @@ const story = {
   argTypes: props,
 };
 
-export const Default = (args: ITabProps) => <TabController {...args} />;
+const Default = (args: ITabProps) => <TabController {...args} />;
 Default.args = {
   id: "thisIsAnId",
   isDisabled: false,
@@ -17,4 +20,18 @@ Default.args = {
   isSelected: { control: null },
 };
 
+const theme = structuredClone(presente);
+
+const Themed = (args: ITabProps) => (
+  <ThemeProvider theme={theme}>
+    <TabController {...args} />
+  </ThemeProvider>
+);
+
+Themed.args = {
+  ...Default.args,
+};
+
 export default story;
+
+export { Default, Themed };
