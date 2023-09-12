52f1d5181b312c501ce4305cbb1b209452c32506
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-09-12 16:50:59 -0500

    feat(Label.styled): implement the interface of the styled-components component

--- src/components/inputs/Label/styles.ts
+++ src/components/inputs/Label/styles.ts
@@ -1,34 +1,38 @@
 import styled from "styled-components";
 
-import { ILabelProps } from ".";
 import { inube } from "@shared/tokens";
-import { Themed } from "./props";
+import { ILabelProps } from ".";
+import { Themed } from "@src/shared/Types/Types";
+
+interface IStyledLabelProps extends ILabelProps {
+  theme?: Themed;
+}
 
 const StyledLabel = styled.label`
-  font-family: ${({ theme }: Themed) => {
+  font-family: ${({ theme }: IStyledLabelProps) => {
     return (
       theme?.typography?.label?.large?.font || inube.typography.label.large.font
     );
   }};
-  font-size: ${({ size, theme }: ILabelProps) =>
+  font-size: ${({ size, theme }: IStyledLabelProps) =>
     size &&
     (theme?.typography?.label?.[size]?.size ||
       inube.typography.label[size].size)};
-  font-weight: ${({ size, theme }: ILabelProps) =>
+  font-weight: ${({ size, theme }: IStyledLabelProps) =>
     size &&
     (theme?.typography?.label?.[size]?.weight ||
       inube.typography.label[size].weight)};
-  letter-spacing: ${({ size, theme }: ILabelProps) =>
+  letter-spacing: ${({ size, theme }: IStyledLabelProps) =>
     size &&
     (theme?.typography?.label?.[size]?.tracking ||
       inube.typography.label[size].tracking)};
-  line-height: ${({ size, theme }: ILabelProps) =>
+  line-height: ${({ size, theme }: IStyledLabelProps) =>
     size &&
     (theme?.typography?.label?.[size]?.lineHeight ||
       inube.typography.label[size].lineHeight)};
-  margin: ${({ margin }: ILabelProps) => margin};
-  padding: ${({ padding }: ILabelProps) => padding};
-  color: ${({ theme, disabled, focused, invalid }: ILabelProps) => {
+  margin: ${({ margin }: IStyledLabelProps) => margin};
+  padding: ${({ padding }: IStyledLabelProps) => padding};
+  color: ${({ theme, disabled, focused, invalid }: IStyledLabelProps) => {
     if (disabled) {
       return (
         theme?.color?.text?.dark?.disabled || inube.color.text.dark.disabled
