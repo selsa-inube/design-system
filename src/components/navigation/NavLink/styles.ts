38217328b5af515703c882f6caad34c7400d4fba
Carlos Andrés Babativa Linares <carlos3k11@gmail.com>
2023-10-05 15:24:31 -0500

    feat(NavLInk): implement the history of Themed

--- src/components/navigation/NavLink/styles.ts
+++ src/components/navigation/NavLink/styles.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import { Themed } from "@shared/types/types";
 import { inube } from "@shared/tokens";
 import { INavLinkProps } from ".";
+
+interface IStyledNavLinkProps extends INavLinkProps {
+  theme?: Themed;
+}
 
 const StyledNavList = styled.li`
   list-style-type: none;
@@ -10,7 +15,7 @@ const StyledNavList = styled.li`
 
 const StyledNavLink = styled.div`
   display: grid;
-  grid-template-columns: ${({ icon }: INavLinkProps) => {
+  grid-template-columns: ${({ icon }: IStyledNavLinkProps) => {
     if (icon) {
       return "auto 1fr auto";
     }
@@ -22,32 +27,49 @@ const StyledNavLink = styled.div`
   min-width: 180px;
   align-items: center;
   box-sizing: border-box;
-  background-size: cover;
-  gap: ${inube.spacing.s300};
-  padding: ${inube.spacing.s0} ${inube.spacing.s200};
-  border-left: ${({ disabled, selected }: INavLinkProps) => {
+  gap: ${({ theme }: IStyledNavLinkProps) =>
+    theme?.spacing?.s300 || inube.spacing.s300};
+  padding: ${({ theme }: IStyledNavLinkProps) =>
+    `${theme?.spacing?.s0 || inube.spacing.s0} ${
+      theme?.spacing?.s200 || inube.spacing.s200
+    }`};
+  border-left: ${({ disabled, selected, theme }: IStyledNavLinkProps) => {
     if (selected && !disabled) {
-      return `5px solid ${inube.color.stroke.dark.regular}`;
+      return `5px solid ${
+        theme?.color?.stroke?.dark?.regular || inube.color.stroke.dark.regular
+      }`;
     }
 
     return `0px`;
   }};
-  background-color: ${({ selected, disabled }: INavLinkProps) => {
+  background-color: ${({ selected, disabled, theme }: IStyledNavLinkProps) => {
     if (disabled) {
-      return inube.color?.surface.navLink.regular;
+      return (
+        theme?.color?.surface?.navLink?.regular ||
+        inube.color.surface.navLink.regular
+      );
     }
     if (selected && !disabled) {
-      return inube.color.surface.navLink.selected;
+      return (
+        theme?.color?.surface?.navLink?.selected ||
+        inube.color.surface.navLink.selected
+      );
     }
-    return inube.color?.surface?.navLink.regular;
+    return (
+      theme?.color?.surface?.navLink?.regular ||
+      inube.color.surface.navLink.regular
+    );
   }};
 
-  ${({ disabled }: INavLinkProps) =>
+  ${({ disabled, theme }: IStyledNavLinkProps) =>
     !disabled &&
     `
     cursor: pointer;
       &:hover {
-        background-color: ${inube.color.surface.navLink.hover};
+        background-color: ${
+          theme?.color?.surface?.navLink?.hover ||
+          inube.color.surface.navLink.hover
+        };
         
       }
   `};
