942d88982c56c4d92fa154c142174a257d841f8f
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-10-04 19:42:18 -0500

    fix(header): remove unnecessary code

--- src/components/navigation/Header/styles.ts
+++ src/components/navigation/Header/styles.ts
@@ -8,32 +8,30 @@ interface IStyledHeaderProps extends IHeaderProps {
 }
 
 const StyledHeader = styled.header`
-  & > div {
-    box-shadow: 0px 1px 3px 1px
-        ${({ theme }: IStyledHeaderProps) =>
-          theme?.color?.stroke?.divider?.regular ||
-          inube.color.stroke.divider.regular},
-      0px 1px 2px 0px
-        ${({ theme }: IStyledHeaderProps) =>
-          theme?.color?.surface.light.disabled ||
-          inube.color.surface.light.disabled};
+  box-shadow: 0px 1px 3px 1px
+      ${({ theme }: IStyledHeaderProps) =>
+        theme?.color?.stroke?.divider?.regular ||
+        inube.color.stroke.divider.regular},
+    0px 1px 2px 0px
+      ${({ theme }: IStyledHeaderProps) =>
+        theme?.color?.surface.light.disabled ||
+        inube.color.surface.light.disabled};
 
-    & > div > div {
-      position: unset;
-      display: flex;
-      align-items: center;
-    }
+  & > div > div > div {
+    position: unset;
+    display: flex;
+    align-items: center;
+  }
 
-    & > div:first-child {
-      padding-left: ${inube.spacing.s150};
-    }
-    & > div:last-child {
-      padding: ${inube.spacing.s100} ${inube.spacing.s200};
-      border-left: 1px solid
-        ${({ theme }: IStyledHeaderProps) =>
-          theme?.color?.stroke?.divider?.regular ||
-          inube.color.stroke.divider.regular};
-    }
+  & > div > div:first-child {
+    padding-left: ${inube.spacing.s150};
+  }
+  & > div > div:last-child {
+    padding: ${inube.spacing.s100} ${inube.spacing.s200};
+    border-left: 1px solid
+      ${({ theme }: IStyledHeaderProps) =>
+        theme?.color?.stroke?.divider?.regular ||
+        inube.color.stroke.divider.regular};
   }
 `;
 
