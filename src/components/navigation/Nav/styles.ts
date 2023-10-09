135f9dba0cdb3acc678c8f840761184663032079
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-10-09 12:32:21 -0500

    fix(nav): added themed-spacing tokens

--- src/components/navigation/Nav/styles.ts
+++ src/components/navigation/Nav/styles.ts
@@ -28,9 +28,13 @@ const StyledFooter = styled.footer`
 
 const SeparatorLine = styled.div`
   width: calc(100% - 32px);
-  margin: ${inube.spacing.s100} ${inube.spacing.s200};
+  margin: ${({ theme }: IStyledNavLinkProps) =>
+    `${theme?.spacing?.s100 || inube.spacing.s100} ${
+      theme?.spacing?.s200 || inube.spacing.s200
+    }`};
   height: 1px;
-  padding: ${inube.spacing.s0} ${inube.spacing.s0};
+  padding: ${({ theme }: IStyledNavLinkProps) =>
+    theme?.spacing?.s0 || inube.spacing.s0};
   background-color: ${({ theme }: IStyledNavLinkProps) =>
     theme?.color?.stroke?.divider?.regular ||
     inube.color.stroke.divider.regular};
