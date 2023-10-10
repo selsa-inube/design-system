0d7c28415f5ecf0c3829d6df1624c7d0675d5579
cesar.marin <cmarin@sistemasenlinea.com.co>
2023-10-09 20:04:50 -0500

    refactor(breadcrumbMenu): use new color tokens

--- src/components/navigation/Breadcrumbs/BreadcrumbMenu/styles.ts
+++ src/components/navigation/Breadcrumbs/BreadcrumbMenu/styles.ts
@@ -1,18 +1,29 @@
 import styled from "styled-components";
-import { colors } from "@shared/colors/colors";
+import { IBreadcrumbMenuProps } from ".";
+import { Themed } from "@shared/types/types";
+import { inube } from "@shared/tokens";
+
+interface IStyledBreadcrumbMenuProps extends IBreadcrumbMenuProps {
+  theme?: Themed;
+}
 
 const StyledBreadcrumbMenu = styled.div`
   position: absolute;
   width: fit-content;
   max-width: 160px;
   min-width: 100px;
-  box-shadow: 0px 2px 4px ${colors.ref.palette.neutralAlpha.n50A};
-  background-color: ${colors.ref.palette.neutral.n0};
+  box-shadow: 0px 2px 4px
+    ${({ theme }: IStyledBreadcrumbMenuProps) =>
+      theme?.color?.stroke?.light?.disabled ||
+      inube.color.stroke.light.disabled};
+  background-color: ${({ theme }: IStyledBreadcrumbMenuProps) =>
+    theme?.color?.stroke?.light?.hover || inube.color.stroke.light.hover};
   border-radius: 4px;
   a {
     &:hover {
       cursor: pointer;
-      background-color: ${colors.ref.palette.neutral.n30};
+      background-color: ${({ theme }: IStyledBreadcrumbMenuProps) =>
+        theme?.color?.surface?.dark?.clear || inube.color.surface.dark.clear};
     }
   }
 `;
