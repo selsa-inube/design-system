aadadf24976b703e70450514e8d5222a0a78f659
Andres Babativa <carlos3k11@gmail.com>
2023-10-11 16:47:07 -0500

    fix(BreadcrumbLink): organize imports

--- src/components/navigation/Breadcrumbs/BreadcrumbLink/styles.ts
+++ src/components/navigation/Breadcrumbs/BreadcrumbLink/styles.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components";
-import { colors } from "@shared/colors/colors";
 import { Link } from "react-router-dom";
+import { Themed } from "@shared/types/types";
+import { colors } from "@shared/colors/colors";
+import { inube } from "@shared/tokens";
 import { IBreadcrumbLinkProps } from "./index";
+
+interface IStyledBreadcrumbLinkProps extends IBreadcrumbLinkProps {
+  themed: Themed;
+}
 
 const StyledContainerLink = styled.li`
   display: inline-block;
@@ -13,7 +19,8 @@ const StyledBreadcrumbLink = styled(Link)`
     props["data-is-active"] ? colors.sys.text.dark : colors.sys.text.secondary};
   &:hover {
     text-decoration: underline;
-    text-decoration-color: ${colors.sys.actions.secondary.stroke};
+    text-decoration-color: ${({ themed }: IStyledBreadcrumbLinkProps) =>
+      themed?.color?.text?.gray?.regular || inube.color.text.gray.regular};
   }
 `;
 
