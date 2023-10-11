890351966531e8480a2e9127b51437d1ade40cf9
Andres Babativa <carlos3k11@gmail.com>
2023-10-11 15:45:03 -0500

    refactor(BreadcrumbLink): adjust the color according to the condition of the component

--- src/components/navigation/Breadcrumbs/BreadcrumbLink/index.tsx
+++ src/components/navigation/Breadcrumbs/BreadcrumbLink/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Text } from "@data/Text";
+
 import { Typos } from "./props";
 import { StyledContainerLink, StyledBreadcrumbLink } from "./styles";
 
@@ -12,11 +14,21 @@ export interface IBreadcrumbLinkProps {
 
 const BreadcrumbLink = (props: IBreadcrumbLinkProps) => {
   const { label, path, id, typo = "large", handleClick } = props;
+  const [appearance, setAppearance] = useState<"gray" | "dark">("gray");
+
+  const onClick = () => {
+    setAppearance("dark");
+    handleClick && handleClick();
+  };
 
   return (
-    <StyledContainerLink id={id} onClick={handleClick}>
+    <StyledContainerLink id={id} onClick={onClick}>
       <Text type="label" size={typo} appearance="gray">
-        <StyledBreadcrumbLink to={path}>{label}</StyledBreadcrumbLink>
+        <StyledBreadcrumbLink to={path}>
+          <Text type="label" size="large" appearance={appearance}>
+            {label}
+          </Text>
+        </StyledBreadcrumbLink>
       </Text>
     </StyledContainerLink>
   );
