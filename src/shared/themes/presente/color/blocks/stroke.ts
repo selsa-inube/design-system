85f27981d75863bc50be7c3e339e6175166269c0
Januar Asprilla <ajanuarandres@gmail.com>
2024-01-29 14:45:46 -0500

    feat: tokens corrections

--- src/shared/themes/presente/color/blocks/stroke.ts
+++ src/shared/themes/presente/color/blocks/stroke.ts
@@ -9,7 +9,7 @@ const stroke = {
   },
   error: {
     regular: palette.red.R400,
-    hover: palette.blue.B300,
+    hover: palette.red.R300,
     disabled: palette.neutral.N70,
   },
   warning: {
@@ -32,7 +32,7 @@ const stroke = {
     hover: palette.purple.P300,
     disabled: palette.neutral.N70,
   },
-  table: {
+  divider: {
     regular: palette.neutral.N40,
   },
   spinner: {
